b4e223d50c0b6f1f53c7d9b3df9020b4afc4f555
jokoho48 <hoffman.jonas95@gmail.com>
2015-12-01 18:02:35 +0100

    remove STMapGestures(ace included)

--- lib/core/cfgFunctions.hpp
+++ lib/core/cfgFunctions.hpp
@@ -6,7 +6,6 @@ class JK_Core {
         class initVaraibleEventHandler { preInit = 1; };
         class triggerMissionEventhandler;
         class addAction;
-        class clientInit {postInit = 1; };
         class buildingPositions;
     };
 };
