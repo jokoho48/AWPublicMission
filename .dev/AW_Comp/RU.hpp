f89839594c20221b424614e951cb342953a9ec3e
jokoho48 <hoffman.jonas95@gmail.com>
2016-01-03 18:56:19 +0100

    finish first AW Comp

--- .dev/AW_Comp/RU.hpp
+++ .dev/AW_Comp/RU.hpp
@@ -19,3 +19,13 @@ class AW_min_rf_soldier_LAT: min_rf_soldier_LAT {
     magazines[] = {"30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","CUP_RPG18_M","SmokeShell","SmokeShellRed","Chemlight_red","Chemlight_red"};
     respawnMagazines[] = {"30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","CUP_RPG18_M","SmokeShell","SmokeShellRed","Chemlight_red","Chemlight_red"};
 };
+class AW_min_rf_soldier_AA: min_rf_soldier_LAT {
+    displayname = "[AW] RU AA";
+    backpack = "min_rf_torna_flora_LAT";
+    linkedItems[] = {"min_rf_helmet_soldier","min_rf_tactical_vest_flora","G_Bandanna_blk","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles_INDEP"};
+    respawnLinkedItems[] = {"min_rf_helmet_soldier","min_rf_tactical_vest_flora","G_Bandanna_blk","ItemMap","ItemCompass","ItemWatch","ItemRadio","NVGoggles_INDEP"};
+    weapons[] = {"arifle_min_rf_ak12_aco","CUP_launch_9K32Strela","hgun_Rook40_F","Throw","Put"};
+    respawnWeapons[] = {"arifle_min_rf_ak12_aco","CUP_launch_9K32Strela","hgun_Rook40_F","Throw","Put"};
+    magazines[] = {"30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","CUP_Strela_2_M","CUP_Strela_2_M","SmokeShell","SmokeShellRed","Chemlight_red","Chemlight_red"};
+    respawnMagazines[] = {"30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","30Rnd_min_rf_545x39_mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","16Rnd_9x21_Mag","CUP_Strela_2_M","CUP_Strela_2_M","SmokeShell","SmokeShellRed","Chemlight_red","Chemlight_red"};
+};
--- .dev/AW_Comp/fnc_randomGear.sqf
+++ .dev/AW_Comp/fnc_randomGear.sqf
@@ -1,23 +1,43 @@
 params ["_unit"];
 
 private _typeOf = typeOf _unit;
-if !(_typeOf in ["AW_ins_AT","AW_ins_MG","AW_ins_GL","AW_ins_RF", "AW_ins_AR"]) exitWith {};
-
+if !(_typeOf in JK_ins_types) exitWith {};
+private _uniform = JK_ins_Uniform call BIS_fnc_selectRandom;
+private _vest = JK_ins_Vests call BIS_fnc_selectRandom;
+private _headgear = JK_ins_HeadGear call BIS_fnc_selectRandom;
+private _backpack = "";
+private _weapon = "";
+private _weaponBack = "";
 
 switch (_typeOf) do {
     case "AW_ins_AT": {
-
+        _backpack = "CUP_B_RPGPack_Khaki";
+        _weaponBack = JK_ins_AT_weapons call BIS_fnc_selectRandom;
+        _weapon = JK_ins_RF_weapons call BIS_fnc_selectRandom;
     };
     case "AW_ins_MG": {
-
+        _backpack = JK_ins_Backpacks call BIS_fnc_selectRandom;
+        _weapon = JK_ins_MG_weapons call BIS_fnc_selectRandom;
     };
     case "AW_ins_GL": {
-
+        _weapon = JK_ins_GL_weapons call BIS_fnc_selectRandom;
     };
     case "AW_ins_RF": {
-
+        _weapon = JK_ins_RF_weapons call BIS_fnc_selectRandom;
     };
     case "AW_ins_AR": {
-
+        _weapon = JK_ins_AR_weapons call BIS_fnc_selectRandom;
     };
+};
+
+_unit forceAddUniform _uniform;
+_unit addVest _vest;
+_unit addHeadgear _headgear;
+if !(_backpack isEqualTo "") then {
+    _unit addBackpack _backpack;
+};
+
+[_unit, _weapon, 5] call BIS_fnc_addWeapon;
+if !(_weaponBack isEqualTo "") then {
+    [_unit, _weaponBack, 2] call BIS_fnc_addWeapon;
 };
